bb69fe515995423107d83471333483677032e5f2
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-18 23:11:43 +0200

    add roll button to roll new dice

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,22 @@ import { useState } from "react";
 import Die from "./components/Die";
 
 export default function App() {
-  const[dice,setDice]=useState(allDice())
-  function allDice(){
-    const newDice = []
-    for (let i=0; i<10;i++){
-      newDice.push(Math.ceil(Math.random()*6))
+  const [dice, setDice] = useState(allDice());
+  function allDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
     }
     return newDice;
   }
-  const diceElement=dice.map(die=> <Die value={die} />)
+  function handleRoll(){
+    setDice(allDice())
+  }
+  const diceElement = dice.map((die) => <Die value={die} />);
   return (
     <div className="slide">
-      <div className="dice-container">
-      {diceElement}
-      </div>
+      <div className="dice-container">{diceElement}</div>
+      <button className="roll-button" onClick={handleRoll}>Roll</button>
     </div>
   );
 }
