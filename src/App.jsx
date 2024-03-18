a1135178f352fe65bc8c789f138d7202be7f67d0
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-18 23:10:43 +0200

    add dice with random number

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+import Die from "./components/Die";
+
 export default function App() {
+  const[dice,setDice]=useState(allDice())
+  function allDice(){
+    const newDice = []
+    for (let i=0; i<10;i++){
+      newDice.push(Math.ceil(Math.random()*6))
+    }
+    return newDice;
+  }
+  const diceElement=dice.map(die=> <Die value={die} />)
   return (
     <div className="slide">
-      <div className="element-container"></div>
+      <div className="dice-container">
+      {diceElement}
+      </div>
     </div>
   );
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,7 @@
+export default function Die(props) {
+  return (
+    <div className="die-item">
+      <h2 className="die-num">{props.value}</h2>
+    </div>
+  );
+}
