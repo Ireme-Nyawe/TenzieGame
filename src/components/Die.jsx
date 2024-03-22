1b6a07d02eb2254a13598f6d6598414c604f794b
Ireme-Nyawe <akimanaj41@gmail.com>
2024-03-22 12:34:49 +0200

    use real dice dot

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -2,9 +2,16 @@ export default function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? "#59E391" : "white",
   };
+
+  let dots = [];
+    for (let i = 0; i < props.value; i++) {
+      dots.push(<span key={i} className="dot"></span>);
+    }
   return (
     <div className="die-item" style={styles} onClick={props.hold}>
-      <h2 className="die-num">{props.value}</h2>
+      <h2 className="die-num">
+        {dots}
+      </h2>
     </div>
   );
 }
